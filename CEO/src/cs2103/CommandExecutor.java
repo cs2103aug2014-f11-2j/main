860e6001aef0d37218f116284c9caad245960238
Yuri <yuri@yuri-x.com>
2014-09-20 15:08:56 +0800

    Basic structure of logic

--- CEO/src/cs2103/CommandExecutor.java
+++ CEO/src/cs2103/CommandExecutor.java
@@ -1,5 +1,55 @@
 package cs2103;
 
-class CommandExecutor {
+import java.util.ArrayList;
+import java.text.DateFormat;  
+import java.util.Date;  
+import java.util.GregorianCalendar;
 
+class CommandExecutor {
+	private ArrayList<Task> taskList;
+	
+	public boolean addTask(String title, String description, String location, String category, String recurrence,  int importance, String startTime, String endTime){
+		//You need to parse the time from String to Date
+		return true;
+	}
+	
+	public ArrayList<Task> listTask(int type){
+		//type: 0-->floating; 1-->deadline; 2-->periodic
+		return this.taskList;
+	}
+	
+	public Task showTaskDetail(int taskID){
+		//
+		return this.taskList.get(taskID);
+	}
+	
+	public boolean deleteTask(int taskID){
+		//
+		taskList.remove(taskID);
+		return true;
+	}
+	
+	public boolean updateTitle(int taskID, String title){
+		return true;
+	}
+	
+	public boolean updateProgress(int taskID, int progress){
+		return true;
+	}
+	
+	public boolean updateDescription(int taskID, String description){
+		return true;
+	}
+	
+	public boolean updateLocation(int taskID, String location){
+		return true;
+	}
+	
+	public boolean updateCategory(int taskID, String category){
+		return true;
+	}
+	
+	public boolean updateType(int taskID, String startTime, String endTime){
+		return true;
+	}
 }
--- CEO/src/cs2103/Task.java
+++ CEO/src/cs2103/Task.java
@@ -10,6 +10,8 @@ class Task {
 	private static enum Type{
 		FLOATING, DEADLINE, PERIOD
 	};
+	private final static byte START=0;
+	private final static byte END=1;
 	private String taskUID;
 	private String title;
 	private String description;
@@ -19,10 +21,10 @@ class Task {
 	private int importance;
 	private Progress progress;
 	private Type type;
-	private Date start;
-	private Date end;
+	private Date[] time;
+
 	
-	public Task(String title, String description, String location, String category, String recurrence,  int importance, String startTime, String endTime) throws CEOException{
+	public Task(String title, String description, String location, String category, String recurrence,  int importance, Date startTime, Date endTime) throws CEOException{
 		if (title!=null){
 			this.title=title;
 			this.description=description;
@@ -35,10 +37,13 @@ class Task {
 				this.type=Type.FLOATING;
 			}else if (endTime==null){
 				this.type=Type.DEADLINE;
-				//Parse time
+				time=new Date[1];
+				this.time[START]=startTime;
 			}else{
 				this.type=Type.PERIOD;
-				//Parse time
+				time=new Date[2];
+				this.time[START]=startTime;
+				this.time[END]=endTime;
 			}
 		}else{
 			throw new CEOException("No Title Error");
@@ -100,17 +105,8 @@ class Task {
 		}
 	}
 	
-	public String getTime(){
-		switch(this.type){
-		case FLOATING:
-			return null;
-		case DEADLINE:
-			return null; //To output time string
-		case PERIOD:
-			return null; //To output combined time string
-		default:
-			return null;
-		}
+	public Date[] getTime(){
+		return this.time;
 	}
 	
 	//To-do: Implement update functions
