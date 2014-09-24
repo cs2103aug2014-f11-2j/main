d2c9b1d663e2d822d553ad85191805bafdf787ab
Yuri <yuri@yuri-x.com>
2014-09-24 16:39:05 +0800

    Added ResponseParser

---
+++ CEO/src/cs2103/ResponseParser.java
@@ -0,0 +1,6 @@
+package cs2103;
+
+public class ResponseParser {
+	//All the methods here should be static
+	//Taking in various response type and convert to Strings and to be displayed in CommandLineUI
+}
