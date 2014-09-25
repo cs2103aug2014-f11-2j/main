91c582aac77fb338d83acd14457e8772fb4b06f6
Brian Luong <bluong@live.unc.edu>
2014-09-25 12:20:46 +0800

    Added / Updated methods in CommandLineUI class

--- CEO/src/cs2103/CommandLineUI.java
+++ CEO/src/cs2103/CommandLineUI.java
@@ -1,13 +1,21 @@
 package cs2103;
 
+import java.util.Scanner;
+
 public class CommandLineUI {
-	private static final String MESSAGE_WELCOME = "Welcome to the CEO. CEO is ready for use";
+	private static final String MESSAGE_WELCOME = "Welcome to the CEO. CEO is ready for use.";
+	private static final String MESSAGE_EXIT = "You have exited CEO. Hope to see you again.";
+	private static final String MESSAGE_USER_PROMPT = "What would you like to do today?";
+	
 	private final CommandParser commandParser;
+	private static Scanner scanner = new Scanner(System.in);
+	
 	public CommandLineUI(String configFile){
 		this.commandParser=new CommandParser(configFile);
 		printWelcomeMessage();
-		promptUserLoop();
+		userLoop();
 	}
+	
 	public static void main(String[] args){
 		CommandLineUI main;
 		if (args.length > 1){
@@ -24,8 +32,23 @@ public class CommandLineUI {
 		System.out.println(MESSAGE_WELCOME);
 	}
 	
-	private static void promptUserLoop() {
-		
+	private static void userLoop() {
+		while (true) {
+			printUserPrompt();
+			takeUserInput();
+			printUserOutput();
+		}
 	}
 	
+	private static void printUserPrompt() {
+		System.out.println(MESSAGE_USER_PROMPT);
+	}
+	
+	private static void takeUserInput() {
+		String userInput = scanner.nextLine();
+	}	
+	
+	private static void printUserOutput() {
+		
+	}
 }
