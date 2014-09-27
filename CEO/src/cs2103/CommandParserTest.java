826ec3b361afedcaae8f39006b2342b234e74771
Brian Luong <bluong@live.unc.edu>
2014-09-27 16:43:00 +0800

    Added More Unit Tests to CommandParser Class

--- CEO/src/cs2103/CommandParserTest.java
+++ CEO/src/cs2103/CommandParserTest.java
@@ -62,6 +62,16 @@ public class CommandParserTest {
 		assertEquals(CommandLineUI.CommandType.INVALID, CommandParser.determineCommandType("hello"));
 	}
 	
+	@Test 
+	public void testDetermineTaskType() {
+		assertEquals(CommandLineUI.TaskType.INVALID, CommandParser.determineTaskType(null));
+		assertEquals(CommandLineUI.TaskType.ALL, CommandParser.determineTaskType("all"));
+		assertEquals(CommandLineUI.TaskType.FLOATING, CommandParser.determineTaskType("floating"));
+		assertEquals(CommandLineUI.TaskType.PERIODIC, CommandParser.determineTaskType("periodic"));
+		assertEquals(CommandLineUI.TaskType.DEADLINE, CommandParser.determineTaskType("deadline"));
+		assertEquals(CommandLineUI.TaskType.INVALID, CommandParser.determineTaskType("hello"));
+	
+	}
 	@Test
 	public void testParseIntegerParameter() {
 		assertEquals(-1, CommandParser.parseIntegerParameter(""));
@@ -70,7 +80,8 @@ public class CommandParserTest {
 		assertEquals(0, CommandParser.parseIntegerParameter("0"));
 		assertEquals(5, CommandParser.parseIntegerParameter("5"));
 		assertEquals(-1, CommandParser.parseIntegerParameter("-1"));
-		assertEquals(-1, CommandParser.parseIntegerParameter("15"));
+		assertEquals(15, CommandParser.parseIntegerParameter("15"));
+		assertEquals(999, CommandParser.parseIntegerParameter("999")); // should there be a limit?
 	}
 	
 }
