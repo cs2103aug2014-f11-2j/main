0b0ca55d8208b9f04285a4a3795900a9d6a3b761
Brian Luong <bluong@live.unc.edu>
2014-10-20 17:59:04 +0800

    Added Task Test Classes

---
+++ CEO/src/cs2103/task/FloatingTaskTest.java
@@ -0,0 +1,169 @@
+package cs2103.task;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class FloatingTaskTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testUpdateComplete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateLocation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateRecurrence() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testConvert() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToSummary() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToDetail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToComponent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckPeriod() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMatches() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFloatingTask() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetComplete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTask() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTaskID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTaskUID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTitle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDescription() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCreated() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLastModified() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateTaskID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateTitle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateDescription() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateLastModified() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCompareTo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddCommonProperty() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDateToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckAlert() {
+		fail("Not yet implemented");
+	}
+
+}
