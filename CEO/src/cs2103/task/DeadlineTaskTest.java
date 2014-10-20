30d6a15b628398dd1f44b16eef4301b56841a053
Brian Luong <bluong@live.unc.edu>
2014-10-20 22:07:36 +0800

    Added Deadline Task Constructor Tests

--- CEO/src/cs2103/task/DeadlineTaskTest.java
+++ CEO/src/cs2103/task/DeadlineTaskTest.java
@@ -2,13 +2,25 @@ package cs2103.task;
 
 import static org.junit.Assert.*;
 
+import java.util.Date;
+
+import net.fortuna.ical4j.model.property.Uid;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import cs2103.exception.HandledException;
+
 public class DeadlineTaskTest {
+	DeadlineTask dlt;
+	Uid taskUID;
+	Date created; 
+	String title;
+	Date dueTime;
+	boolean complete;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -21,11 +33,57 @@ public class DeadlineTaskTest {
 	@Before
 	public void setUp() throws Exception {
 	}
-
+	
 	@After
 	public void tearDown() throws Exception {
+		
 	}
-
+	@Test 
+	public void testDeadlineTaskConstruction(){
+		testDeadlineTaskConstructionOne();
+		testDeadlineTaskConstructionTwo();
+		testDeadlineTaskConstructionThree();
+	}
+	
+	public void testDeadlineTaskConstructionOne() {
+		try{
+			dlt=new DeadlineTask(null,null,null,null,false);
+			fail("Expected- Handled Exception");
+		} catch(HandledException e){
+			assertEquals(e.printErrorMsg(),"A Non-empty title must be specified!");
+		}
+	}
+	
+	
+	public void testDeadlineTaskConstructionTwo() {
+		try{
+			dlt=new DeadlineTask(null,null," ",null,false);
+			fail("Expected- Handled Exception");
+		} catch(HandledException e){
+			assertEquals(e.printErrorMsg(),"Your input time cannot be parsed, please check your input and try again!");
+		}
+	}
+	
+	@SuppressWarnings("deprecation")
+	public void testDeadlineTaskConstructionThree() {
+		try{
+			dlt=new DeadlineTask(null,null," ",new Date(2014,10,10),false);
+			assertTrue(true);
+		} catch(HandledException e){
+			fail("Expected- Succesful Creation");
+		}
+	}
+	
+	@Test
+	public void testDeadlineTaskOne() throws HandledException{
+		dlt=new DeadlineTask(null,null,null,null,false);
+		testAllMethods(dlt);
+	}
+	
+	public void testAllMethods(DeadlineTask dltToTest){
+		
+	}
+	
 	@Test
 	public void testUpdateComplete() {
 		fail("Not yet implemented");
