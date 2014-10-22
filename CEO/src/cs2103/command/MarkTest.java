38ca32a3d947dac9467c5d16f779d13ff0f8391d
crispyfridge <tan.c.chunhui@gmail.com>
2014-10-22 21:28:11 +0800

    added @Test for MarkTest.java and updated some incorrect code

--- CEO/src/cs2103/command/MarkTest.java
+++ CEO/src/cs2103/command/MarkTest.java
@@ -10,8 +10,7 @@ import cs2103.exception.HandledException;
 
 public class MarkTest {
 
-	@Test
-	public void testMarkFloating() throws HandledException, FatalException {
+	private void initialise() throws HandledException, FatalException{
 		TaskList.getInstance(null, false);
 		Add addObj = new Add("add -title floating");
 		addObj.execute();
@@ -19,23 +18,28 @@ public class MarkTest {
 		addObj.execute();
 		addObj = new Add("add -title periodic -time 2014/10/23 20:20 to 2014/10/25 20:20");
 		addObj.execute();
-		
+	}
+	
+	@Test
+	public void testMarkFloating() throws HandledException, FatalException {
+		initialise();
 		Mark markFloating = new Mark("1");
 		assertEquals("Successfully marked 1 as completed",markFloating.execute());
 	}
 	
+	@Test
 	public void testMarkPeriodic() throws HandledException, FatalException{
 		TaskList.getInstance();
-		
-		Mark markPeriodic = new Mark("2");
-		assertEquals("The task you specified does not contain status information",markPeriodic.execute());
+		Mark markDeadline = new Mark("3");
+		assertEquals("The task you specified does not contain status information",markDeadline.execute());
 	}
 	
+	@Test
 	public void testMarkDeadline() throws HandledException, FatalException{
-		TaskList.getInstance();
-		
-		Mark markDeadline = new Mark("3");
-		assertEquals("Successfully marked 3 as completed",markDeadline.execute());
+		initialise();
+		//cos for some reason the entire task list is wiped/nulled after testMarkPeriodic
+		Mark markPeriodic = new Mark("2");
+		assertEquals("Successfully marked 2 as completed",markPeriodic.execute());
 	}
 	
 }
