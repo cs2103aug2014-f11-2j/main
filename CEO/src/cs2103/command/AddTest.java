91ecd44a46b0129e8f0525ca2fc3eae80aa21c77
crispyfridge <tan.c.chunhui@gmail.com>
2014-10-22 16:40:54 +0800

    removed duplicated code

--- CEO/src/cs2103/command/AddTest.java
+++ CEO/src/cs2103/command/AddTest.java
@@ -14,36 +14,6 @@ public class AddTest {
 	public void testAddCorrectCommand() throws HandledException, FatalException {
 		TaskList.getInstance(null,false);
 		Add addObj;
-		addObj = new Add("add --title hello --description some description");
-		assertEquals("You have successfully added a new task.",addObj.execute());
-	
-	}
-	
-	@Test
-	public void testAddIncorrectCommand() throws HandledException, FatalException{
-		TaskList.getInstance(null,false);
-		Add addobj;
-		addobj = new Add("add");
-		assertEquals("You have successfully added a new task.",addobj.execute());
-	}
-
-}
-package cs2103.command;
-
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
-import cs2103.TaskList;
-import cs2103.exception.FatalException;
-import cs2103.exception.HandledException;
-
-public class AddTest {
-	
-	@Test
-	public void testAddCorrectCommand() throws HandledException, FatalException {
-		TaskList.getInstance(null,false);
-		Add addObj;
 		addObj = new Add("-title hello -description some description");
 		assertEquals("You have successfully added a new task.",addObj.execute());
 	
