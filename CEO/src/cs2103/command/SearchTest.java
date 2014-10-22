0633884cb8e8d439d18829d36fa0ff1904f1bb06
crispyfridge <tan.c.chunhui@gmail.com>
2014-10-22 22:06:39 +0800

    added missing @Test and added testSearchDeadline

--- CEO/src/cs2103/command/SearchTest.java
+++ CEO/src/cs2103/command/SearchTest.java
@@ -10,8 +10,7 @@ import cs2103.exception.HandledException;
 
 public class SearchTest {
 	
-	@Test
-	public void testSearchKeyword() throws HandledException, FatalException {
+	private void initialise() throws HandledException, FatalException{
 		TaskList.getInstance(null, false);
 		Add addObj = new Add("-title Jap homework -time 2014/10/23 21:00");
 		addObj.execute();
@@ -23,27 +22,65 @@ public class SearchTest {
 		addObj.execute();
 		addObj = new Add("-title finish essay");
 		addObj.execute();
+	}
 		
+	@Test
+	public void testSearchKeyword() throws HandledException, FatalException {	
+		try{
+			TaskList.getInstance();
+		}catch(FatalException e){
+			initialise();
+		}
 		Search searchTitle = new Search("all -keyword homework");
+		String result = searchTitle.execute();
 		assertEquals("1. Jap homework\n" +
 				"Type: Deadline	Status: Needs Action	Due At: 23-Oct-2014 21:00:00\n" +
 				"2. revise stats homework\n" +
-				"Type: Periodic	From: 24-Oct-2014 20:00:00 To 25-Oct-2014 20:00:00",searchTitle.execute());
+				"Type: Periodic	From: 24-Oct-2014 20:00:00 To 25-Oct-2014 20:00:00",result);
 	}
 	
-	public void testSearchIgnoreCase() throws FatalException, HandledException{
-		TaskList.getInstance();
-		Search searchIgnoreCase = new Search("all -keyword nus");
-		assertEquals("2. revise stats homework\n" +
-				"Type: Periodic	From: 24-Oct-2014 20:00:00 To 25-Oct-2014 20:00:00",searchIgnoreCase.execute());
-	}
 	
+	@Test
 	public void testSearchComplete() throws FatalException, HandledException{
-		TaskList.getInstance();
+		try{
+			TaskList.getInstance();
+		}catch(FatalException e){
+			initialise();
+		}
 		Mark markComplete = new Mark("1");
 		markComplete.execute();
 		Search searchComplete = new Search("all -complete");
+		String result = searchComplete.execute();
+		markComplete.undo();
 		assertEquals("1. Jap homework\n" +
-				"Type: Deadline	Status: Completed	Due At: 23-Oct-2014 21:00:00",searchComplete.execute());
+				"Type: Deadline	Status: Completed	Due At: 23-Oct-2014 21:00:00",result);
+	}
+	
+	@Test
+	public void testSearchPeriodic() throws HandledException, FatalException{
+		try{
+			TaskList.getInstance();
+		}catch(FatalException e){
+			initialise();
+		}
+		Search searchPeriodic = new Search("periodic");
+		String result = searchPeriodic.execute();
+		assertEquals("2. revise stats homework\n" +
+				"Type: Periodic	From: 24-Oct-2014 20:00:00 To 25-Oct-2014 20:00:00",result);
+	}
+	
+	@Test
+	public void testSearchDeadline() throws HandledException, FatalException{
+		try{
+			TaskList.getInstance();
+		}catch(FatalException e){
+			initialise();
+		}
+		Search searchDeadline = new Search("deadline");
+		String result = searchDeadline.execute();
+		assertEquals("1. Jap homework\n" +
+				"Type: Deadline	Status: Needs Action	Due At: 23-Oct-2014 21:00:00\n" +
+				"3. celebrate 21st :D\n" +
+				"Type: Deadline	Status: Needs Action	Due At: 01-Nov-2014 18:00:00",result);
 	}
 }
