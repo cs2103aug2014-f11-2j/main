e82f017cc95782005cb2a79e341164f902571825
crispyfridge <tan.c.chunhui@gmail.com>
2014-10-22 11:21:33 +0800

    added test case for delete

---
+++ CEO/src/cs2103/command/CommandTest.java
@@ -0,0 +1,14 @@
+package cs2103.command;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CommandTest {
+
+	@Test
+	public void testCommand() {
+		Command c = new Command();
+	}
+
+}
