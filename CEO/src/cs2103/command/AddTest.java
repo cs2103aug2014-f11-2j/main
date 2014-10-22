e502ad40afa36ee16599b630af6eb1c7bb911e69
crispyfridge <tan.c.chunhui@gmail.com>
2014-10-22 08:03:56 +0800

    getInstance of tasklist so test cases won't call FatalException

--- CEO/src/cs2103/command/AddTest.java
+++ CEO/src/cs2103/command/AddTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import cs2103.TaskList;
 import cs2103.exception.FatalException;
 import cs2103.exception.HandledException;
 
@@ -11,6 +12,7 @@ public class AddTest {
 	
 	@Test
 	public void testAddCorrectCommand() throws HandledException, FatalException {
+		TaskList.getInstance(null,false);
 		Add addObj;
 		addObj = new Add("add --title hello --description some description");
 		assertEquals("You have successfully added a new task.",addObj.execute());
@@ -19,6 +21,37 @@ public class AddTest {
 	
 	@Test
 	public void testAddIncorrectCommand() throws HandledException, FatalException{
+		TaskList.getInstance(null,false);
+		Add addobj;
+		addobj = new Add("add");
+		assertEquals("You have successfully added a new task.",addobj.execute());
+	}
+
+}
+package cs2103.command;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import cs2103.TaskList;
+import cs2103.exception.FatalException;
+import cs2103.exception.HandledException;
+
+public class AddTest {
+	
+	@Test
+	public void testAddCorrectCommand() throws HandledException, FatalException {
+		TaskList.getInstance(null,false);
+		Add addObj;
+		addObj = new Add("add --title hello --description some description");
+		assertEquals("You have successfully added a new task.",addObj.execute());
+	
+	}
+	
+	@Test
+	public void testAddIncorrectCommand() throws HandledException, FatalException{
+		TaskList.getInstance(null,false);
 		Add addobj;
 		addobj = new Add("add");
 		assertEquals("You have successfully added a new task.",addobj.execute());
