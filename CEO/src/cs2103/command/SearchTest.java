2d8c1ef8f0bec3f869f74f58bc1dd4878f867a3e
crispyfridge <tan.c.chunhui@gmail.com>
2014-10-23 19:11:22 +0800

    used @BeforeClass, added invalidInput test case

--- CEO/src/cs2103/command/SearchTest.java
+++ CEO/src/cs2103/command/SearchTest.java
@@ -2,6 +2,7 @@ package cs2103.command;
 
 import static org.junit.Assert.*;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import cs2103.TaskList;
@@ -10,7 +11,8 @@ import cs2103.exception.HandledException;
 
 public class SearchTest {
 	
-	private void initialise() throws HandledException, FatalException{
+	@BeforeClass
+	public static void initialise() throws HandledException, FatalException{
 		TaskList.getInstance(null, false);
 		Add addObj = new Add("-title Jap homework -time 2014/10/23 21:00");
 		addObj.execute();
@@ -22,15 +24,15 @@ public class SearchTest {
 		addObj.execute();
 		addObj = new Add("-title finish essay");
 		addObj.execute();
+		addObj = new Add("-title complete");
+		addObj.execute();
+		Mark m = new Mark("6");
+		m.execute();
 	}
 		
 	@Test
 	public void testSearchKeyword() throws HandledException, FatalException {	
-		try{
-			TaskList.getInstance();
-		}catch(FatalException e){
-			initialise();
-		}
+		
 		Search searchTitle = new Search("all -keyword homework");
 		String result = searchTitle.execute();
 		assertEquals("1. Jap homework\n" +
@@ -42,27 +44,16 @@ public class SearchTest {
 	
 	@Test
 	public void testSearchComplete() throws FatalException, HandledException{
-		try{
-			TaskList.getInstance();
-		}catch(FatalException e){
-			initialise();
-		}
-		Mark markComplete = new Mark("1");
-		markComplete.execute();
+		
 		Search searchComplete = new Search("all -complete");
 		String result = searchComplete.execute();
-		markComplete.undo();
-		assertEquals("1. Jap homework\n" +
-				"Type: Deadline	Status: Completed	Due At: 23-Oct-2014 21:00:00",result);
+		assertEquals("6. complete\n" +
+				"Type: Floating	Status: Completed",result);
 	}
 	
 	@Test
 	public void testSearchPeriodic() throws HandledException, FatalException{
-		try{
-			TaskList.getInstance();
-		}catch(FatalException e){
-			initialise();
-		}
+		
 		Search searchPeriodic = new Search("periodic");
 		String result = searchPeriodic.execute();
 		assertEquals("2. revise stats homework\n" +
@@ -71,11 +62,7 @@ public class SearchTest {
 	
 	@Test
 	public void testSearchDeadline() throws HandledException, FatalException{
-		try{
-			TaskList.getInstance();
-		}catch(FatalException e){
-			initialise();
-		}
+		
 		Search searchDeadline = new Search("deadline");
 		String result = searchDeadline.execute();
 		assertEquals("1. Jap homework\n" +
@@ -86,26 +73,20 @@ public class SearchTest {
 	
 	@Test
 	public void testSearchFloating() throws HandledException, FatalException{
-		try{
-			TaskList.getInstance();
-		}catch(FatalException e){
-			initialise();
-		}
+		
 		Search searchFloating = new Search("floating");
 		String result = searchFloating.execute();
 		assertEquals("4. do cs2105 networking assignment\n" +
 				"Type: Floating	Status: Needs Action\n" +
 				"5. finish essay\n" +
-				"Type: Floating	Status: Needs Action",result);
+				"Type: Floating	Status: Needs Action\n" +
+				"6. complete\n" +
+				"Type: Floating	Status: Completed",result);
 	}
 	
 	@Test
 	public void testSearchTime() throws HandledException, FatalException{
-		try{
-			TaskList.getInstance();
-		}catch(FatalException e){
-			initialise();
-		}
+		
 		Search searchTime = new Search("all -time");
 		String result = searchTime.execute();
 		assertEquals("1. Jap homework\n" +
@@ -115,4 +96,5 @@ public class SearchTest {
 				"3. celebrate 21st :D\n" +
 				"Type: Deadline	Status: Needs Action	Due At: 01-Nov-2014 18:00:00",result);
 	}
+	
 }
--- CEO/src/cs2103/command/ShowTest.java
+++ CEO/src/cs2103/command/ShowTest.java
@@ -2,6 +2,7 @@ package cs2103.command;
 
 import static org.junit.Assert.*;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import cs2103.TaskList;
@@ -9,12 +10,16 @@ import cs2103.exception.FatalException;
 import cs2103.exception.HandledException;
 
 public class ShowTest {
-
-	@Test
-	public void testShow() throws HandledException, FatalException {
+	@BeforeClass
+	public static void initialise() throws HandledException, FatalException{
 		TaskList.getInstance(null, false);
 		Add addObj = new Add("-title testShow -time 2014/10/23 20:20 to 2014/10/25 10:00 -location home -description much testing, such title");
 		addObj.execute();
+	}
+	
+	@Test
+	public void testShow() throws HandledException, FatalException {
+		
 		Show show = new Show("1");
 		String result = show.execute();
 		assertEquals("The details for Task 1:\n" +
@@ -22,6 +27,12 @@ public class ShowTest {
 				"Type: Periodic	From: 23-Oct-2014 20:20:00 To 25-Oct-2014 10:00:00\n" +
 				"Location: home\n" +
 				"Description: much testing, such title",result);
+	}
+	
+	@Test(expected = HandledException.class)
+	public void testInvalidShow() throws HandledException, FatalException{
+		Show show = new Show("2");
+		show.execute();
 	}
 
 }
