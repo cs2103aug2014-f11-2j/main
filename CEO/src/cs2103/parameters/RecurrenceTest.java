e49141a27670d242022511c5121bd156c6ced722
crispyfridge <tan.c.chunhui@gmail.com>
2014-10-23 15:26:31 +0800

    updated testcase for testRecurrence

--- CEO/src/cs2103/parameters/RecurrenceTest.java
+++ CEO/src/cs2103/parameters/RecurrenceTest.java
@@ -2,6 +2,8 @@ package cs2103.parameters;
 
 import static org.junit.Assert.*;
 
+import java.text.ParseException;
+
 import org.junit.Test;
 
 import cs2103.exception.HandledException;
@@ -9,9 +11,10 @@ import cs2103.exception.HandledException;
 public class RecurrenceTest {
 
 	@Test
-	public void testRecurrence() throws HandledException {
+	public void testRecurrence() throws HandledException, ParseException {
 		Recurrence rec = Recurrence.parse("2d");
 		assertEquals("RECURRENCE",rec.getType());
+		assertEquals("FREQ=DAILY;INTERVAL=2",rec.getValue().toString());
 	}
 
 }
