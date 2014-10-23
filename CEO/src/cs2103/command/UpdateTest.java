81ed29c7e938be718a06089119a64042cb3f2fbd
crispyfridge <tan.c.chunhui@gmail.com>
2014-10-23 19:39:07 +0800

    used @BeforeClass, added Invalid input test for Update, made small changes for UpdateTimeFromRecur

--- CEO/src/cs2103/command/UpdateTest.java
+++ CEO/src/cs2103/command/UpdateTest.java
@@ -2,6 +2,7 @@ package cs2103.command;
 
 import static org.junit.Assert.*;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import cs2103.TaskList;
@@ -9,7 +10,9 @@ import cs2103.exception.FatalException;
 import cs2103.exception.HandledException;
 
 public class UpdateTest {
-	private void initialise() throws HandledException, FatalException{
+	
+	@BeforeClass
+	public static void initialise() throws HandledException, FatalException{
 		TaskList.getInstance(null, false);
 		Add addObj = new Add("-title updateTest");
 		addObj.execute();
@@ -17,17 +20,37 @@ public class UpdateTest {
 
 	@Test
 	public void testUpdate() throws HandledException, FatalException {
-		try{
-			TaskList.getInstance();
-		}catch(FatalException e){
-			initialise();
-		}
+		
 		Update updateTitle = new Update("1 -title I was wrong");
 		String result = updateTitle.execute();
 		assertEquals("You have updated task with ID 1",result);
-		List listAll = new List("all");
+		List list = new List("floating");
+		result = list.execute();
+		
 		assertEquals("1. I was wrong\n" +
-				"Type: Floating	Status: Needs Action",listAll.execute());
+				"Type: Floating	Status: Needs Action",result);
 	}
-
+	
+	@Test(expected = HandledException.class)
+	public void testInvalidUpdate() throws HandledException, FatalException{
+		Update updateInvalid = new Update("-title this is wrong, so much wrong");
+		updateInvalid.execute();
+	}
+	
+	@Test
+	public void testUpdateRedoUndo() throws HandledException, FatalException{
+		Add addObj = new Add("-title muchness title -time 2014/11/29 20:20");
+		addObj.execute();
+		Update updateRedo = new Update("2 -title much wrong, very title");
+		updateRedo.execute();
+		updateRedo.undo();
+		List list = new List("deadline");
+		String result = list.execute();
+		assertEquals("2. muchness title\n" +
+				"Type: Deadline	Status: Needs Action	Due At: 29-Nov-2014 20:20:00",result);
+		updateRedo.redo();
+		result = list.execute();
+		assertEquals("2. much wrong, very title\n" +
+				"Type: Deadline	Status: Needs Action	Due At: 29-Nov-2014 20:20:00",result);
+	}
 }
--- CEO/src/cs2103/command/UpdateTimeFromRecurTest.java
+++ CEO/src/cs2103/command/UpdateTimeFromRecurTest.java
@@ -2,6 +2,7 @@ package cs2103.command;
 
 import static org.junit.Assert.*;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import cs2103.TaskList;
@@ -10,19 +11,16 @@ import cs2103.exception.HandledException;
 
 public class UpdateTimeFromRecurTest {
 
-	private void initialise() throws HandledException, FatalException{
+	@BeforeClass
+	public static void initialise() throws HandledException, FatalException{
 		TaskList.getInstance(null, false);
-		Add addObj = new Add("-title updateFromRecur -time 2014/10/18 23:00 to 2014/10/20 23:00 -recurring 2d");
+		Add addObj = new Add("-title updateFromRecur -time 2014/10/18 23:00 to 2014/10/20 23:00 -recur 2d");
 		addObj.execute();
 	}
 	
 	@Test
 	public void testUpdateFromRecur() throws HandledException, FatalException {
-		try{
-			TaskList.getInstance();
-		}catch(FatalException e){
-			initialise();
-		}
+		
 		UpdateTimeFromRecur ufr = new UpdateTimeFromRecur();
 		String result = ufr.execute();
 		assertEquals("Successfully updated 1 recurring tasks",result);
