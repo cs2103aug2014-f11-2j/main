7c22791310229ba94fd8e3b0f695f73defb941c7
Yuri <yuri@yuri-x.com>
2014-10-28 14:24:27 +0800

    Simplify code

--- CEO/lib/ConsoleClear/cs2103_util_Console.c
+++ CEO/lib/ConsoleClear/cs2103_util_Console.c
@@ -6,12 +6,9 @@
 extern "C" {
 #endif
 	JNIEXPORT void JNICALL Java_cs2103_util_Console_clr(JNIEnv *env, jobject thisObj){
- 
        HANDLE chwnd = GetStdHandle(STD_OUTPUT_HANDLE);
- 
        COORD coordScreen = { 0, 0 };
        DWORD cCharsWritten;
- 
        {
            WORD textColours;
            CONSOLE_SCREEN_BUFFER_INFO *consoleInfo = new CONSOLE_SCREEN_BUFFER_INFO();
@@ -19,27 +16,14 @@ extern "C" {
            textColours = consoleInfo->wAttributes;
            SetConsoleTextAttribute(chwnd, textColours);
        }
- 
        CONSOLE_SCREEN_BUFFER_INFO csbi;
        DWORD dwConSize;
- 
-       if( !GetConsoleScreenBufferInfo( chwnd, &csbi ))
-          return;
+       if( !GetConsoleScreenBufferInfo( chwnd, &csbi )) return;
        dwConSize = csbi.dwSize.X * csbi.dwSize.Y;
- 
-       if( !FillConsoleOutputCharacter( chwnd, (TCHAR) ' ',
-          dwConSize, coordScreen, &cCharsWritten ))
-          return;
- 
-       if( !GetConsoleScreenBufferInfo( chwnd, &csbi ))
-          return;
- 
-       if( !FillConsoleOutputAttribute( chwnd, csbi.wAttributes,
-          dwConSize, coordScreen, &cCharsWritten ))
-          return;
- 
+       if( !FillConsoleOutputCharacter( chwnd, (TCHAR) ' ', dwConSize, coordScreen, &cCharsWritten )) return;
+       if( !GetConsoleScreenBufferInfo( chwnd, &csbi )) return;
+       if( !FillConsoleOutputAttribute( chwnd, csbi.wAttributes, dwConSize, coordScreen, &cCharsWritten )) return;
        SetConsoleCursorPosition( chwnd, coordScreen );
- 
        return;
 	}
  
