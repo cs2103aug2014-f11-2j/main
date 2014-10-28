a40aaa5e3afcec03b80fdb784f974ff9afd5987f
Yuri <yuri@yuri-x.com>
2014-10-28 14:31:21 +0800

    Fix JNI build script

--- CEO/lib/ConsoleClear/compile.sh
+++ CEO/lib/ConsoleClear/compile.sh
@@ -1,2 +1,4 @@
 x86_64-w64-mingw32-g++ -Wall -march=x86-64 -static-libgcc -static-libstdc++ -D_JNI_IMPLEMENTATION_ -Wl,--kill-at -I"C:/Program Files/Java/jdk1.8.0_20/include" -I"C:/Program Files/Java/jdk1.8.0_20/include/win32" -shared cs2103_util_Console.c -o ConsoleClear_x64.dll
 i686-w64-mingw32-g++ -Wall -march=i686 -static-libgcc -static-libstdc++ -D_JNI_IMPLEMENTATION_ -Wl,--kill-at -I"C:/Program Files/Java/jdk1.8.0_20/include" -I"C:/Program Files/Java/jdk1.8.0_20/include/win32" -shared cs2103_util_Console.c -o ConsoleClear_x86.dll
+upx -9 ConsoleClear_x64.dll
+upx -9 ConsoleClear_x86.dll
