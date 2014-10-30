666dfac1e4e81be7cdb64ec2925a4f5b3b4bd9c9
Yuri <yuri@yuri-x.com>
2014-10-30 10:38:14 +0800

    Run script for Windows, Mac and linux

--- CEO/lib/ConsoleClear/compile.sh
+++ CEO/lib/ConsoleClear/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Can only be compiled under cygwin64 with mingw w64 gcc package installed. JDK path is absolute hence might need editing before compilation
-x86_64-w64-mingw32-g++ -Wall -march=x86-64 -static-libgcc -static-libstdc++ -D_JNI_IMPLEMENTATION_ -Wl,--kill-at -I"C:/Program Files/Java/jdk1.7.0_71/include" -I"C:/Program Files/Java/jdk1.7.0_71/include/win32" -shared cs2103_util_Console.c -o ../ConsoleClear_x64.dll
-i686-w64-mingw32-g++ -Wall -march=i686 -static-libgcc -static-libstdc++ -D_JNI_IMPLEMENTATION_ -Wl,--kill-at -I"C:/Program Files/Java/jdk1.7.0_71/include" -I"C:/Program Files/Java/jdk1.7.0_71/include/win32" -shared cs2103_util_Console.c -o ../ConsoleClear_x86.dll
+x86_64-w64-mingw32-g++ -Wall -march=x86-64 -static-libgcc -static-libstdc++ -D_JNI_IMPLEMENTATION_ -Wl,--kill-at -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/win32" -shared cs2103_util_Console.c -o ../ConsoleClear_x64.dll
+i686-w64-mingw32-g++ -Wall -march=i686 -static-libgcc -static-libstdc++ -D_JNI_IMPLEMENTATION_ -Wl,--kill-at -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/win32" -shared cs2103_util_Console.c -o ../ConsoleClear_x86.dll
 ./upx -9 ../ConsoleClear_x64.dll
 ./upx -9 ../ConsoleClear_x86.dll
