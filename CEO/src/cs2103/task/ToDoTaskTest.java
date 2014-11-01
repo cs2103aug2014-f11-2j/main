bde3330fd68189c49a462a96a6b0bb4bab0be9a1
Brian Luong <bluong@live.unc.edu>
2014-11-01 18:04:22 +0800

    Refactored Task Testing More

--- CEO/src/cs2103/task/ToDoTaskTest.java
+++ CEO/src/cs2103/task/ToDoTaskTest.java
@@ -1,29 +1,37 @@
 package cs2103.task;
 
+import static org.fusesource.jansi.Ansi.Color.GREEN;
+import static org.fusesource.jansi.Ansi.Color.RED;
 import static org.junit.Assert.*;
+
+import org.fusesource.jansi.Ansi;
+
+import static org.fusesource.jansi.Ansi.*;
+import static org.fusesource.jansi.Ansi.Color.*;
 
 import java.util.Date;
 
 import net.fortuna.ical4j.model.DateTime;
 import net.fortuna.ical4j.model.property.Status;
 
+import org.fusesource.jansi.*;
 import org.junit.Test;
 
 public abstract class ToDoTaskTest extends TaskTest{
 
 	@Test
 	public void testUpdateAndGetCompleted(){
-		Task task = getConcrete();
+		ToDoTask task = (ToDoTask) getConcrete();
 		task.updateCompleted(null);
 		assertEquals(null, task.getCompleted());
-		Date testDate = new DateTime();
+		DateTime testDate = new DateTime();
 		task.updateCompleted(testDate);
 		assertTrue(testDate.equals(task.getCompleted()));
 	}
 	
 	@Test
 	public void testUpdateAndGetStatus(){
-		Task task = getConcrete();
+		ToDoTask task = (ToDoTask) getConcrete();
 		task.updateStatus(null);
 		assertEquals(Status.VTODO_NEEDS_ACTION, task.getStatus());
 		String testStatus = "Test Status";
@@ -32,18 +40,18 @@ public abstract class ToDoTaskTest extends TaskTest{
 	
 	@Test
 	public void testDeleteAndIsDeleted(){
-		Task task = getConcrete();
+		ToDoTask task = (ToDoTask) getConcrete();
 		task.delete();
 		assertTrue(task.isDeleted());
 	}
 	
 	@Test
 	public void testRestore(){
-		Task task = getConcrete();
+		ToDoTask task = (ToDoTask) getConcrete();
 		task.updateCompleted(null);
 		task.restore();
 		assertEquals(Status.VTODO_NEEDS_ACTION, task.getStatus());
-		Date testDate = new DateTime();
+		DateTime testDate = new DateTime();
 		task.updateCompleted(testDate);
 		task.restore();
 		assertEquals(Status.VTODO_COMPLETED, task.getStatus());
@@ -51,5 +59,13 @@ public abstract class ToDoTaskTest extends TaskTest{
 	}
 	
 	@Test
+	public void testCompletedToString(){
+		ToDoTask task = (ToDoTask) getConcrete();
+		Ansi testAnsi = ansi().fg(RED).a("Needs Action").reset(); 
+		assertEquals(testAnsi, task.completedToString(null));
+		DateTime testDate = new DateTime();
+		testAnsi = ansi().fg(GREEN).a("Completed").reset(); 
+		assertEquals(testAnsi, task.completedToString(testDate));
+	}
 	
 }
