0a98078131510c9ed565e4c3fe44439baf9b29be
crispyfridge <tan.c.chunhui@gmail.com>
2014-11-03 01:45:17 +0800

    unsuccessful sync test

---
+++ CEO/src/cs2103/command/SyncTest.java
@@ -0,0 +1,29 @@
+package cs2103.command;
+
+import static org.fusesource.jansi.Ansi.ansi;
+import static org.fusesource.jansi.Ansi.Color.GREEN;
+import static org.fusesource.jansi.Ansi.Color.MAGENTA;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import cs2103.exception.FatalException;
+import cs2103.exception.HandledException;
+
+public class SyncTest {
+
+	@Test
+	public void testSync() throws HandledException, FatalException {
+		Sync s = new Sync(null);
+		String result = s.execute().toString();
+		assertEquals(ansi().fg(GREEN).a("Successfully sync your data with Google\n").reset().toString(),result);
+	}
+	
+	@Test
+	public void testDiableSync() throws HandledException, FatalException{
+		Sync s = new Sync("-disable");
+		String result = s.execute().toString();
+		assertEquals(ansi().fg(MAGENTA).a("You have disabled sync with Google\n").reset().toString(),result);
+	}
+
+}
