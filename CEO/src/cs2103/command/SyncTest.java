b6e25ef864afe7c09f7282face0b46b4b98c354a
crispyfridge <tan.c.chunhui@gmail.com>
2014-11-03 08:14:48 +0800

    added TaskList.getInstance and checkSyncSupport

--- CEO/src/cs2103/command/SyncTest.java
+++ CEO/src/cs2103/command/SyncTest.java
@@ -5,15 +5,25 @@ import static org.fusesource.jansi.Ansi.Color.GREEN;
 import static org.fusesource.jansi.Ansi.Color.MAGENTA;
 import static org.junit.Assert.*;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import cs2103.exception.FatalException;
 import cs2103.exception.HandledException;
+import cs2103.parameters.Option;
+import cs2103.storage.TaskList;
+import cs2103.util.CommonUtil;
 
 public class SyncTest {
 
+	@BeforeClass
+	public static void initialise() throws HandledException, FatalException{
+		TaskList.getInstance(new Option(Option.Value.TEST));
+	}
+	
 	@Test
-	public void testSync() throws HandledException, FatalException {
+	public void testEnableSync() throws HandledException, FatalException {
+		assertTrue(CommonUtil.checkSyncSupport());
 		Sync s = new Sync(null);
 		String result = s.execute().toString();
 		assertEquals(ansi().fg(GREEN).a("Successfully sync your data with Google\n").reset().toString(),result);
