8aab531fab12c7b08d9b3cc19d381e0f090865e8
Yuri <yuri@yuri-x.com>
2014-11-07 21:24:44 +0800

    add getTestInstance()

--- CEO/src/cs2103/storage/TaskList.java
+++ CEO/src/cs2103/storage/TaskList.java
@@ -273,6 +273,15 @@ public class TaskList {
 		this.google = null;
 	}
 	
+	/**
+	 * @return a new Test instance of TaskList
+	 * @throws FatalException
+	 * @throws HandledException
+	 */
+	public static TaskList getTestInstance() throws FatalException, HandledException {
+		return new TaskList(new Option(Option.Value.TEST));
+	}
+	
 	private void commitDeleteToGoogle(Task task) throws HandledException {
 		try {
 			if (this.google.needToSync(task)) {
