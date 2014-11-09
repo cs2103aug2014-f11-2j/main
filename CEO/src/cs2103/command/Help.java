bc8bbc6a9616e652a7d14a24b0eb05fb8b16256e
Yuri <yuri@yuri-x.com>
2014-11-10 00:24:52 +0800

    Urgent update of help

--- CEO/src/cs2103/command/Help.java
+++ CEO/src/cs2103/command/Help.java
@@ -9,7 +9,7 @@ import static org.fusesource.jansi.Ansi.*;
 import static org.fusesource.jansi.Ansi.Color.*;
 
 public class Help extends QueryCommand {
-	public static final Ansi HELP_DEFAULT = ansi().fg(YELLOW).a("CEO Usage:\n").reset()
+	private static final Ansi HELP_DEFAULT = ansi().fg(YELLOW).a("CEO Usage:\n").reset()
             								  .bold().a("  add <Quick add string>\n" +
             										    "      ([-S or --title <title>] [-D or -description <description>]\n" + 
             										    "      [-L or -location <location>] [-R or -recurring <number h/d/w/m/y>]\n" +
@@ -18,9 +18,11 @@ public class Help extends QueryCommand {
             								  .bold().a("  list (<floating|deadline|periodic|all|trash>)\n").reset()
             							   .bg(GREEN).a("  +List existing tasks. Enter \"help list\" for more\n\n").reset()
             								  .bold().a("  show <task ID>\n").reset()
-            							   .bg(GREEN).a("  +Show detail of the task with specified task ID. Enter \"help show\" for more\n\n").reset()
+            							   .bg(GREEN).a("  +Show detail of the task with specified task ID.\n\n").reset()
             								  .bold().a("  delete <task ID> (-p)\n").reset()
             								 .bg(RED).a("  +Delete task with corresponding taskID. Enter \"help delete\" for more\n\n").reset()
+            								  .bold().a("  restore <task ID>\n").reset()
+            								.bg(BLUE).a("  +restore a task from trash bin.\n\n").reset()
             								  .bold().a("  update <task ID> ([-S or -title <title>] [-C or -complete {true|false}]\n" +
             										  	"                   [-D or -description <description>]\n" +
             										  	"                   [-L or -location <location>]\n" +
@@ -28,14 +30,14 @@ public class Help extends QueryCommand {
             										  	"                   [-R or -recurring <number h/d/w/m/y>])\n").reset()
             								.bg(BLUE).a("  +Update task with corresponding task ID. Enter \"help update\" for more\n\n").reset()
             								  .bold().a("  undo/redo <number of steps>\n").reset()
-            								.bg(BLUE).a("  +Undo/redo some steps. Enter \"help undo\" or \"help redo\" for more\n\n").reset()
+            								.bg(BLUE).a("  +Undo/redo some steps.\n\n").reset()
             								  .bold().a("  search (<keyword String>)\n" +
             								  "         {([-K or -type <floating|deadline|periodic|all|trash>]\n" +
             								  "         [-C or -complete {true|false}]\n" +
             								  "         [-T or -time {<blank>|<time>|<<time> to <time>>}]}\n").reset()
             								.bg(GREEN).a("  +Search for tasks. Enter \"help search\" for more\n\n").reset()
             								  .bold().a("  mark <task ID>\n").reset()
-            								.bg(BLUE).a("  Mark a task as completed. Enter \"help mark\" for more\n\n").reset()
+            								.bg(BLUE).a("  Mark a task as completed.\n\n").reset()
             								  .bold().a("  sync (-disable)\n").reset()
             								.bg(BLUE).a("  +Manually sync your task with Google. In usual circumstances, CEO will\n" +
             											"   automatically sync with Google, this command is useful when you disabled\n" +
@@ -45,7 +47,7 @@ public class Help extends QueryCommand {
             								.bg(BLUE).a("  +display approaching deadlines and start times\n\n").reset()
             								  .bold().a("  help\n").reset()
             								.bg(BLUE).a("  +display this message\n").reset();
-	public static final Ansi HELP_ADD = ansi().a("Add usage:\n")
+	private static final Ansi HELP_ADD = ansi().a("Add usage:\n")
 							    .bold().a("  add <Quick add string>\n" +
 										  "      ([-S or --title <title>] [-D or -description <description>]\n" + 
 										  "      [-L or -location <location>] [-R or -recurring <number h/d/w/m/y>]\n" +
@@ -73,13 +75,13 @@ public class Help extends QueryCommand {
 							.bg(YELLOW).a("  add -title Task Title -description Describe this task -location office\n" +
 										  "      -time 2014/10/12 14:22 to 2014/10/13 14:22 -recurring 1w\n").reset()
 							  .bg(BLUE).a("This will effectively adding a Periodic task with title \"Task Title\", with description \"Describe this task\", with location \"office\", with a time period from 2014/10/12 14:22 to 2014/10/13 14:22, and this task will recur every 1 week\n").reset();
-	public static final Ansi HELP_DELETE = ansi().a("Delete usage:\n")
+	private static final Ansi HELP_DELETE = ansi().a("Delete usage:\n")
 								   .bold().a("  delete <task ID> (-p)\n").reset()
 										  .a("Options:\n")
 						         .bg(CYAN).a("  <task ID>        Elementary, specify the task you want to move to trash\n" +
 										  	 "  -p               Optional, the task will be removed permanantly\n").reset()
 						       .bg(YELLOW).a("Example: delete 1\n").reset();
-	public static final Ansi HELP_UPDATE = ansi().a("Update usage:\n")
+	private static final Ansi HELP_UPDATE = ansi().a("Update usage:\n")
 								   .bold().a("  update <task ID> ([-S or -title <title>] [-C or -complete {true|false}]\n" +
 											 "                   [-D or -description <description>]\n" +
 											 "                   [-L or -location <location>]\n" +
@@ -105,7 +107,7 @@ public class Help extends QueryCommand {
 			 			       .bg(YELLOW).a("  update 1 -title Task Title -description Describe this task\n" +
 			 								 "           -time 2014/10/12 -complete true\n").reset()
 			 				     .bg(BLUE).a("This will effectively update the task with Task ID 1 to a Deadline task with title \"Task Title\", with description \"Describe this task\", with a deadline at 2014/10/12 and indicate the task is complete\n").reset();
-	public static final Ansi HELP_LIST = ansi().a("List usage:\n")
+	private static final Ansi HELP_LIST = ansi().a("List usage:\n")
 								 .bold().a("list (<<blank>|floating|deadline|periodic|all>)\n" +
 										   "  <blank>              list only incomplete tasks\n" +
 										   "  floating             list tasks with no dates set\n" +
@@ -114,13 +116,7 @@ public class Help extends QueryCommand {
 										   "  all                  list all tasks\n" +
 										   "  trash                list all tasks in trash bin\n").reset()
 							 .bg(YELLOW).a("Example:\nlist deadline\n").reset();
-	public static final Ansi HELP_SHOW = ansi().bold().a("Show has no extra options\n" +
-										   "Example:\nshow 1\n").reset();
-	public static final Ansi HELP_REDO = ansi().bold().a("Redo has no extra options\n" +
-										   "Example:\nredo 1\n").reset();
-	public static final Ansi HELP_UNDO = ansi().bold().a("Undo has no extra options\n" +
-										   "Example:\nundo 1\n").reset();
-	public static final Ansi HELP_SEARCH = ansi().a("Search usage:\n")
+	private static final Ansi HELP_SEARCH = ansi().a("Search usage:\n")
 								   .bold().a("  search (<keyword String>)\n" +
 											 "         {([-K or -type <floating|deadline|periodic|all|trash>]\n" +
 											 "         [-C or -complete {true|false}]\n" +
@@ -139,8 +135,6 @@ public class Help extends QueryCommand {
 			 							  .a("Example:\n")
 			 			       .bg(YELLOW).a("  search example keyword -keyword all -complete true -time\n").reset()
 			 			         .bg(BLUE).a("This will effectively return the completed Deadline tasks which title, description or location contains \"example keyword\". (when complete parameter is specified, it is implied that all Periodic Tasks will be excluded)\n").reset();
-	public static final Ansi HELP_MARK = ansi().bold().a("Mark has no extra options\n" +
-			 							   "Example: mark 2\n").reset();
 	
 	/**
 	 * Creates an instance of Help from String command
@@ -162,16 +156,8 @@ public class Help extends QueryCommand {
 			return HELP_ADD;
 		case DELETE:
 			return HELP_DELETE;
-		case SHOW:
-			return HELP_SHOW;
-		case UNDO:
-			return HELP_UNDO;
-		case REDO:
-			return HELP_REDO;
 		case SEARCH:
 			return HELP_SEARCH;
-		case MARK:
-			return HELP_MARK;
 		case INVALID:
 		default:
 			return HELP_DEFAULT;
--- CEO/src/cs2103/command/HelpTest.java
+++ CEO/src/cs2103/command/HelpTest.java
@@ -15,7 +15,7 @@ import org.junit.Test;
 import cs2103.exception.HandledException;
 
 public class HelpTest {
-	public static final Ansi HELP_DEFAULT = ansi().fg(YELLOW).a("CEO Usage:\n").reset()
+	private static final Ansi HELP_DEFAULT = ansi().fg(YELLOW).a("CEO Usage:\n").reset()
 			  .bold().a("  add <Quick add string>\n" +
 					    "      ([-S or --title <title>] [-D or -description <description>]\n" + 
 					    "      [-L or -location <location>] [-R or -recurring <number h/d/w/m/y>]\n" +
@@ -24,9 +24,11 @@ public class HelpTest {
 			  .bold().a("  list (<floating|deadline|periodic|all|trash>)\n").reset()
 		   .bg(GREEN).a("  +List existing tasks. Enter \"help list\" for more\n\n").reset()
 			  .bold().a("  show <task ID>\n").reset()
-		   .bg(GREEN).a("  +Show detail of the task with specified task ID. Enter \"help show\" for more\n\n").reset()
+		   .bg(GREEN).a("  +Show detail of the task with specified task ID.\n\n").reset()
 			  .bold().a("  delete <task ID> (-p)\n").reset()
 			 .bg(RED).a("  +Delete task with corresponding taskID. Enter \"help delete\" for more\n\n").reset()
+			  .bold().a("  restore <task ID>\n").reset()
+			.bg(BLUE).a("  +restore a task from trash bin.\n\n").reset()
 			  .bold().a("  update <task ID> ([-S or -title <title>] [-C or -complete {true|false}]\n" +
 					  	"                   [-D or -description <description>]\n" +
 					  	"                   [-L or -location <location>]\n" +
@@ -34,14 +36,14 @@ public class HelpTest {
 					  	"                   [-R or -recurring <number h/d/w/m/y>])\n").reset()
 			.bg(BLUE).a("  +Update task with corresponding task ID. Enter \"help update\" for more\n\n").reset()
 			  .bold().a("  undo/redo <number of steps>\n").reset()
-			.bg(BLUE).a("  +Undo/redo some steps. Enter \"help undo\" or \"help redo\" for more\n\n").reset()
+			.bg(BLUE).a("  +Undo/redo some steps.\n\n").reset()
 			  .bold().a("  search (<keyword String>)\n" +
 			  "         {([-K or -type <floating|deadline|periodic|all|trash>]\n" +
 			  "         [-C or -complete {true|false}]\n" +
 			  "         [-T or -time {<blank>|<time>|<<time> to <time>>}]}\n").reset()
 			.bg(GREEN).a("  +Search for tasks. Enter \"help search\" for more\n\n").reset()
 			  .bold().a("  mark <task ID>\n").reset()
-			.bg(BLUE).a("  Mark a task as completed. Enter \"help mark\" for more\n\n").reset()
+			.bg(BLUE).a("  Mark a task as completed.\n\n").reset()
 			  .bold().a("  sync (-disable)\n").reset()
 			.bg(BLUE).a("  +Manually sync your task with Google. In usual circumstances, CEO will\n" +
 						"   automatically sync with Google, this command is useful when you disabled\n" +
@@ -51,7 +53,7 @@ public class HelpTest {
 			.bg(BLUE).a("  +display approaching deadlines and start times\n\n").reset()
 			  .bold().a("  help\n").reset()
 			.bg(BLUE).a("  +display this message\n").reset();
-public static final Ansi HELP_ADD = ansi().a("Add usage:\n")
+private static final Ansi HELP_ADD = ansi().a("Add usage:\n")
 .bold().a("  add <Quick add string>\n" +
 		  "      ([-S or --title <title>] [-D or -description <description>]\n" + 
 		  "      [-L or -location <location>] [-R or -recurring <number h/d/w/m/y>]\n" +
@@ -79,13 +81,13 @@ public static final Ansi HELP_ADD = ansi().a("Add usage:\n")
 .bg(YELLOW).a("  add -title Task Title -description Describe this task -location office\n" +
 		  "      -time 2014/10/12 14:22 to 2014/10/13 14:22 -recurring 1w\n").reset()
 .bg(BLUE).a("This will effectively adding a Periodic task with title \"Task Title\", with description \"Describe this task\", with location \"office\", with a time period from 2014/10/12 14:22 to 2014/10/13 14:22, and this task will recur every 1 week\n").reset();
-public static final Ansi HELP_DELETE = ansi().a("Delete usage:\n")
+private static final Ansi HELP_DELETE = ansi().a("Delete usage:\n")
  .bold().a("  delete <task ID> (-p)\n").reset()
 		  .a("Options:\n")
 .bg(CYAN).a("  <task ID>        Elementary, specify the task you want to move to trash\n" +
 		  	 "  -p               Optional, the task will be removed permanantly\n").reset()
 .bg(YELLOW).a("Example: delete 1\n").reset();
-public static final Ansi HELP_UPDATE = ansi().a("Update usage:\n")
+private static final Ansi HELP_UPDATE = ansi().a("Update usage:\n")
  .bold().a("  update <task ID> ([-S or -title <title>] [-C or -complete {true|false}]\n" +
 			 "                   [-D or -description <description>]\n" +
 			 "                   [-L or -location <location>]\n" +
@@ -111,7 +113,7 @@ public static final Ansi HELP_UPDATE = ansi().a("Update usage:\n")
 .bg(YELLOW).a("  update 1 -title Task Title -description Describe this task\n" +
 			 "           -time 2014/10/12 -complete true\n").reset()
 .bg(BLUE).a("This will effectively update the task with Task ID 1 to a Deadline task with title \"Task Title\", with description \"Describe this task\", with a deadline at 2014/10/12 and indicate the task is complete\n").reset();
-public static final Ansi HELP_LIST = ansi().a("List usage:\n")
+private static final Ansi HELP_LIST = ansi().a("List usage:\n")
 .bold().a("list (<<blank>|floating|deadline|periodic|all>)\n" +
 		   "  <blank>              list only incomplete tasks\n" +
 		   "  floating             list tasks with no dates set\n" +
@@ -120,13 +122,7 @@ public static final Ansi HELP_LIST = ansi().a("List usage:\n")
 		   "  all                  list all tasks\n" +
 		   "  trash                list all tasks in trash bin\n").reset()
 .bg(YELLOW).a("Example:\nlist deadline\n").reset();
-public static final Ansi HELP_SHOW = ansi().bold().a("Show has no extra options\n" +
-		   "Example:\nshow 1\n").reset();
-public static final Ansi HELP_REDO = ansi().bold().a("Redo has no extra options\n" +
-		   "Example:\nredo 1\n").reset();
-public static final Ansi HELP_UNDO = ansi().bold().a("Undo has no extra options\n" +
-		   "Example:\nundo 1\n").reset();
-public static final Ansi HELP_SEARCH = ansi().a("Search usage:\n")
+private static final Ansi HELP_SEARCH = ansi().a("Search usage:\n")
  .bold().a("  search (<keyword String>)\n" +
 			 "         {([-K or -type <floating|deadline|periodic|all|trash>]\n" +
 			 "         [-C or -complete {true|false}]\n" +
@@ -145,6 +141,7 @@ public static final Ansi HELP_SEARCH = ansi().a("Search usage:\n")
 		  .a("Example:\n")
 .bg(YELLOW).a("  search example keyword -keyword all -complete true -time\n").reset()
 .bg(BLUE).a("This will effectively return the completed Deadline tasks which title, description or location contains \"example keyword\". (when complete parameter is specified, it is implied that all Periodic Tasks will be excluded)\n").reset();
+
 public static final Ansi HELP_MARK = ansi().bold().a("Mark has no extra options\n" +
 		   "Example: mark 2\n").reset();
 
@@ -180,32 +177,8 @@ public static final Ansi HELP_MARK = ansi().bold().a("Mark has no extra options\
 	}
 	
 	@Test
-	public void testShowHelp() throws HandledException{
-		Help showHelp = new Help("show");
-		assertEquals(HELP_SHOW.toString(),showHelp.execute().toString());
-	}
-	
-	@Test
-	public void testRedoHelp() throws HandledException{
-		Help redoHelp = new Help("redo");
-		assertEquals(HELP_REDO.toString(),redoHelp.execute().toString());
-	}
-	
-	@Test
-	public void testUndoHelp() throws HandledException{
-		Help undoHelp = new Help("undo");
-		assertEquals(HELP_UNDO.toString(), undoHelp.execute().toString());
-	}
-	
-	@Test
 	public void testSearchHelp() throws HandledException{
 		Help searchHelp = new Help("search");
 		assertEquals(HELP_SEARCH.toString(), searchHelp.execute().toString());
-	}
-	
-	@Test
-	public void testMarkHelp() throws HandledException{
-		Help markHelp = new Help("mark");
-		assertEquals(HELP_MARK.toString(), markHelp.execute().toString());
 	}
 }
